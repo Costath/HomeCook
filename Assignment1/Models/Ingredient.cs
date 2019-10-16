62d4c8405534ee806455fb0c71c4182a532b6a0d
Costath <thalesdaniel@gmail.com>
2019-10-15 22:57:44 -0400

    Added Ingredients Class

---
+++ Assignment1/Models/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Ingredient
+    {
+        public int IngredientID { get; set; }
+        public string Name { get; set; }
+        public float Quantity { get; set; }
+        public _unitOfMeasurement UnitOfMeasurement { get; set; }
+        public enum _unitOfMeasurement
+        {
+            litter,
+            ml,
+            gram,
+            kg,
+            oz,
+            fl_oz,
+            pound,
+            tbsp,
+            tsp
+        }
+    }
+}
